2673ee7b0218ddde7e5abbd12779d07c33fe8c49
ZHAO Yukan <ucandoit@zhaotekiair.home>
2017-11-04 17:36:28 +0100

    add react router and bootstrap

--- src/App.js
+++ src/App.js
@@ -1,20 +1,44 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route} from 'react-router-dom';
+import {Navbar, Nav, NavItem} from 'react-bootstrap';
+import {LinkContainer} from 'react-router-bootstrap';
+import Dashboard from './components/dashboard';
+import Cats from './components/cats';
+import Skills from './components/skills';
 import './App.css';
 
 class App extends Component {
   render() {
-    return (
+    return (<Router>
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Navbar collapseOnSelect="collapseOnSelect">
+          <Navbar.Header>
+            <Navbar.Brand>
+              <LinkContainer to="/">
+                <a>Nobunyaga</a>
+              </LinkContainer>
+            </Navbar.Brand>
+            <Navbar.Toggle/>
+          </Navbar.Header>
+          <Navbar.Collapse>
+            <Nav>
+              <LinkContainer to="/cats">
+                <NavItem eventKey={1}>Cats</NavItem>
+              </LinkContainer>
+              <LinkContainer to="/skills">
+                <NavItem eventKey={2}>Skills</NavItem>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Navbar>
+
+        <Route exact="exact" path="/" component={Dashboard}/>
+        <Route path="/cats" component={Cats}/>
+        <Route path="/skills" component={Skills}/>
+
+        <div>Developed by @Ucandoit - v0.0.1</div>
       </div>
-    );
+    </Router>);
   }
 }
 
---
+++ src/components/cats.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react';
+
+class Cats extends Component {
+  render() {
+    return <h1>Cats</h1>;
+  }
+}
+
+export default Cats;
